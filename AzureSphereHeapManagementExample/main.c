995c88b4274fc39c168d99510baabe58477eb3e7
JawaharT <Jtunuguntla@gmail.com>
2021-08-23 09:54:02 +0100

    Section 2 Heap Management Example

---
+++ AzureSphereHeapManagementExample/main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int* number = (int*)malloc(sizeof(int)); // Can be performed for other data types too 
+
+    // Check if malloc has succeeded 
+    if (number != NULL) {
+        // Succeeded, assign value and continue program as normal 
+        *number = 99;
+    }else {
+        // Failed, exit program and log the issue
+        return 0;
+    }
+
+    // Free allocated memory
+    if (number != NULL) {
+        free(number);
+        number = NULL;
+    }
+    return 0;
+}
